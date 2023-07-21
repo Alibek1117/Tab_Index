3abe12129bac0c6bd83010e36c52e6716ebd2880
Alibek <alibek00@mail.com>
2023-07-21 19:47:52 +0500

    added smth

--- src/assets/components/Modal.jsx
+++ src/assets/components/Modal.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Modal = ({closeModal}) => {
   return (
-    <div className='modal-backdrop'>
+    <div onClick={closeModal} className='modal-backdrop'>
         <div className="modal">
            <h3>Not Found</h3>
            <button onClick={closeModal}  className="btnn">Close</button>
